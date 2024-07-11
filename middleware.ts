4c37edde5243a4f5a165a20854487f3b106b5bdb
Nabid <nabid.explore@gmail.com>
2024-07-11 15:00:02 +0600

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isProtectedRoute = createRouteMatcher(["/", "/all-notes"]);
+const isProtectedRoute = createRouteMatcher(["/", "/all-notes"]);
 
 export default clerkMiddleware((auth, req) => {
-  // if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
